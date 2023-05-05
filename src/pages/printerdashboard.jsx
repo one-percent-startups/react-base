c09461cabca3c4d4bee04c83d6ac01f446d8c550
Rishabh Mehta <47055064+MehtaRishabh47@users.noreply.github.com>
2023-05-05 18:48:14 +0530

    file upload modal Ui

--- src/pages/printerdashboard.jsx
+++ src/pages/printerdashboard.jsx
@@ -1420,8 +1420,23 @@ const Dashboard = () => {
                       )}
                     </Formik> */}
                     <form onSubmit={handleFileUpload}>
-                      <input type="file" onChange={handleFileChange} />
-                      <button type="submit">Upload</button>
+                      <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                        <ArrowUpTrayIcon
+                          className="h-6 w-6 "
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="mt-3 text-center sm:mt-5">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          Upload File
+                        </Dialog.Title>
+                      </div>
+
+                      <input type="file" onChange={handleFileChange} className="relative mt-5 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary" />
+                      <button type="submit" className="mt-5 inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Upload</button>
                     </form>
                   </Dialog.Panel>
                 </Transition.Child>
