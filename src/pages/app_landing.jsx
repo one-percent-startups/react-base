dd99c2291f2c60fc519895fca8919ad71af60999
Rishabh Mehta <47055064+MehtaRishabh47@users.noreply.github.com>
2023-05-31 14:39:54 +0530

    added scrollanimation

--- src/pages/app_landing.jsx
+++ src/pages/app_landing.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { useRef, useState, Fragment, useEffect } from "react";
 import { AppNavbar } from "../components/navbar/AppNav";
 import { Dialog, Transition } from "@headlessui/react";
 import {
@@ -27,13 +27,20 @@ import {
 import { CheckIcon } from "@heroicons/react/20/solid";
 
 export const AppLanding = () => {
+  const sectionsRef = useRef([]);
   const [open, setOpen] = useState(false);
+
+
+
   return (
-    <div className="bg-white scroll-smooth">
-      <div className="bg-[#DFE7FF] ">
+    <div className="bg-white scroll-smooth scroller">
+      <div className="bg-[#DFE7FF]  Appsection ">
         <AppNavbar />
 
-        <div className="max-w-[1280px] mx-auto px-5 md:px-0">
+        <div
+          className="max-w-[1280px] mx-auto px-5 md:px-0"
+          ref={(ref) => (sectionsRef.current[0] = ref)}
+        >
           <div className="mt-20 text-center pb-20">
             <h4 className="text-primary_blue md:text-normal text-sm font-bold capitalize">
               LAUNCHING IN SEPTEMBER 2023
@@ -65,7 +72,10 @@ export const AppLanding = () => {
         </div>
       </div>
 
-      <div className="bg-dark_blue flex flex-col justify-center items-center py-32 px-5 md:px-0">
+      <div
+        className="bg-dark_blue flex flex-col justify-center items-center py-32 px-5 md:px-0 Appsection"
+        ref={(ref) => (sectionsRef.current[1] = ref)}
+      >
         <h1 className="md:text-6xl text-2xl text-center text-white">
           Optimize your <span className="text-primary_blue">Income</span>.
           <br className="md:block hidden"></br>
@@ -89,7 +99,11 @@ export const AppLanding = () => {
         </button>
       </div>
 
-      <div className="bg-white relative text-6xl max-w-[1280px] mx-auto md:flex justify-end z-1 px-5 md:px-0">
+      <div
+        className="bg-white relative text-6xl max-w-[1280px] mx-auto md:flex justify-end z-1 px-5 md:px-0 Appsection"
+        id="scrollAnimate"
+        ref={(ref) => (sectionsRef.current[2] = ref)}
+      >
         <div className="md:absolute md:left-0 md:w-6/12 md:translate-y-2/3">
           <div className="flex md:items-start items-end justify-center pt-10 md:flex-col flex-row ">
             <h1 className="mt-4 text-black lg:text-8xl md:text-7xl text-4xl font-semibold inline-flex items-end ">
@@ -165,7 +179,10 @@ export const AppLanding = () => {
         </div>
       </div>
 
-      <div className="bg-dark_blue pt-32 md:pb-56 pb-20 px-5 md:px-0 mt-16 md:mt-0">
+      <div
+        className="bg-dark_blue pt-32 md:pb-56 pb-20 px-5 md:px-0 mt-16 md:mt- Appsection"
+        ref={(ref) => (sectionsRef.current[3] = ref)}
+      >
         <div className="max-w-[1280px] mx-auto text-center">
           <h3 className="text-primary_blue">Get started</h3>
           <h1 className="md:text-7xl md:text-5xl text-4xl font-bold text-white mt-5">
@@ -243,7 +260,10 @@ export const AppLanding = () => {
         </div>
       </div>
 
-      <div className="bg-dark_blue  md:block hidden">
+      <div
+        className="bg-dark_blue  md:block hidden Appsection"
+        ref={(ref) => (sectionsRef.current[4] = ref)}
+      >
         <div className="bg-primary_blue pt-32">
           <div className="max-w-[1280px] mx-auto flex justify-between items-center">
             <div className="w-3/12">
@@ -271,7 +291,10 @@ export const AppLanding = () => {
         </div>
       </div>
 
-      <div className="bg-dark_blue md:pt-20 px-5 md:px-0">
+      <div
+        className="bg-dark_blue md:pt-20 px-5 md:px-0 Appsection"
+        ref={(ref) => (sectionsRef.current[5] = ref)}
+      >
         <div className="max-w-[1280px] mx-auto ">
           <div className="md:flex justify-between items-start">
             <div className="md::w-4/12 mb-10 pt-10 md:pt-0">
